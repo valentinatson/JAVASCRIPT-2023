436e92aa566cd4364b76d158a38dbd8b7fba1c13
zartan <atsonvalentin283@gmail.com>
2023-08-08 10:34:52 +0000

    end of variable

---
+++ partie1/index.js
@@ -0,0 +1,35 @@
+/* console.log("hello world") //pour afficher qlq chose dans la console
+
+alert("hello world")  //pour afficher un alerte
+alert("1255") */
+
+let nom  // declaration de variable ; on peut avoir nom1, nom_etu, nomEtudiant mais never let 1 ou let 1nom
+nom = "Mohamed"  // affectation d'une valeur à la 
+
+// camelcase
+let nomEtudiant = "Reddington"
+
+const nombreDevie = 100  //pour declarer des variables qui ne changent pas
+
+var prenom = "Ezebio"// mauvaise pratique du au hosting
+
+
+console.log(nom, nomEtudiant, nombreDevie);
+
+
+/* let age = prompt("quelle est votre age ?") // pour demander a l utilisateurd entrer une valeur, l'affecte au variable age et l afiche dans la console
+console.log(age); */
+
+
+    //exercice
+
+//let name = prompt("quel est votre nom?")
+let username = prompt("quel est votre prenom?")
+alert("bienvenue" + username)
+//console.log("bienvenue" + username);
+
+
+
+                // structure conditionnelle
+
+
